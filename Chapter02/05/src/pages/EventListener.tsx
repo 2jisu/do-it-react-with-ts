1cfe1f5422e85fcd2e056eaf5bcc03772dba8b74
2jisu <jisu.jjang88@gmail.com>
2024-01-16 17:16:58 +0900

    [ADD] 02-05장 실습 코드 추가 WIP

--- Chapter02/05/src/pages/EventListener.tsx
+++ Chapter02/05/src/pages/EventListener.tsx
@@ -1,3 +1,15 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+// 이벤트 리스너 (이벤트 처리기): 이벤트가 발생할 때까지 귀 기울여 기다리다가(콜백 함수) 이벤트가 발생하면 해당 이벤트를 코드 쪽으로 알려주는 역할을 수행
+// 타입스크립트의 옵셔널 체이닝 연산자 (?.): 해당 메서드가 null을 반환할 경우 뒤쪽의 메서드를 호출하지 않음.
+// 물리 DOM 객체의 이벤트 속성
+document.getElementById('root')?.addEventListener('click', (e: Event) => {
+  const {isTrusted, target, bubbles} = e
+  console.log('mouse click occurs.', isTrusted, target, bubbles)
+})
+document.getElementById('root')?.addEventListener('click', (e: Event) => {
+  const {isTrusted, target, bubbles} = e
+  console.log('mouse click also occurs.', isTrusted, target, bubbles)
+})
+
+export default function EventListener() {
+  return <div>EventListener</div>
 }
--- Chapter02/05/src/pages/OnClick.tsx
+++ Chapter02/05/src/pages/OnClick.tsx
@@ -1,3 +1,19 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+// 물리 DOM 객체의 이벤트 속성
+// 옵셔널 체이닝 연산자는 'on이벤트명' 으로 값을 설정하는 구문에서는 사용 불가
+// 'on이벤트명' 으로 설정한 이벤트는 가장 마지막에 설정된 콜백 함수를 호출함.
+const rootDiv = document.getElementById('root')
+if (rootDiv) {
+  rootDiv.onclick = (e: Event) => {
+    const {isTrusted, target, bubbles} = e
+    console.log('mouse click occurs on rootDiv', isTrusted, target, bubbles)
+  }
+  rootDiv.onclick = (e: Event) => {
+    const {isTrusted, target, bubbles} = e
+    // prettier-ignore
+    console.log('mouse click also occurs on rootDiv', isTrusted, target, bubbles)
+  }
+}
+
+export default function onClick() {
+  return <div>OnClick</div>
 }
