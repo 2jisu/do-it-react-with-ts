1faa2e7cec08c38d0570ecfb5cb58e0e87fe6b11
2jisu <jisu.jjang88@gmail.com>
2024-01-18 01:00:12 +0900

    [MODIFY] 02-01장 실습 코드 수정

--- Chapter02/01/src/index.tsx
+++ Chapter02/01/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// 물리 DOM
+// 물리 DOM 객체
 let pPhysicalDOM = document.createElement('p')
 pPhysicalDOM.innerText = 'Hello physical DOM wolrd!'
 document.body.appendChild(pPhysicalDOM)
 
-// 가상 DOM
+// 가상 DOM 객체
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const pVirtualDOM = React.createElement('p', null, 'Hello virtual DOM world!')
 // root.render: 가상 DOM 을 물리 DOM 으로 전환해 주는 기능 (물리 DOM 의 appendChild 와 같은 기능), 가상 DOM 객체의 렌더링을 수행
