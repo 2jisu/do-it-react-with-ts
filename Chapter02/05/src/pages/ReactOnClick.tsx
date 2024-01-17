70a2920c4a375b30221bc7797226285ba1bbc0ba
2jisu <jisu.jjang88@gmail.com>
2024-01-17 18:23:32 +0900

    [ADD] 02-05장 실습 코드 추가 WIP

--- Chapter02/05/src/pages/ReactOnClick.tsx
+++ Chapter02/05/src/pages/ReactOnClick.tsx
@@ -1,3 +1,16 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import {SyntheticEvent} from 'react'
+
+// 리액트 컴포넌트의 이벤트 속성
+// html 요소의 이벤트 속성: 모두 소문자, 리액트 컴포넌트의 이벤트 속성: 카멜 케이스
+export default function ReactOnClick() {
+  const onClick = (e: SyntheticEvent) => {
+    const {isTrusted, target, bubbles} = e
+    console.log('mouse click occurs on <button>', isTrusted, target, bubbles)
+  }
+  return (
+    <div>
+      <p>ReactOnClick</p>
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  )
 }
