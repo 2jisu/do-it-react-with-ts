677c15b0ba9accc2e2c286674d77e3cf1ef44488
2jisu <jisu.jjang88@gmail.com>
2024-01-18 01:12:14 +0900

    [UPDATE] 03-01장 실습 코드 WIP

--- Chapter03/01/src/App.tsx
+++ Chapter03/01/src/App.tsx
@@ -1,29 +1,18 @@
-import DispatchEvent from './pages/DispatchEvent'
-import DragDrop from './pages/DragDrop'
-import EventBubbling from './pages/EventBubbling'
-import EventListener from './pages/EventListener'
-import FileDrop from './pages/FileDrop'
-import FileInput from './pages/FileInput'
-import OnChange from './pages/OnChange'
-import OnClick from './pages/OnClick'
-import ReactOnClick from './pages/ReactOnClick'
-import StopPropagation from './pages/StopPropagation'
-import VariousInputs from './pages/VariousInputs'
+import Bootstrap from './pages/Bootstrap'
+import Icon from './pages/Icon'
+import Style from './pages/Style'
+import UsingIcon from './pages/UsingIcon'
+import UsingIconWithCssClass from './pages/UsingIconWithCssClass'
+import './App.css'
 
 export default function App() {
   return (
     <div>
-      <FileDrop />
-      <DragDrop />
-      <FileInput />
-      <OnChange />
-      <VariousInputs />
-      <StopPropagation />
-      <EventBubbling />
-      <DispatchEvent />
-      <ReactOnClick />
-      <OnClick />
-      <EventListener />
+      <Bootstrap />
+      <Icon />
+      <Style />
+      <UsingIcon />
+      <UsingIconWithCssClass />
     </div>
   )
 }
