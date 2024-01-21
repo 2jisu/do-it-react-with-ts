a4920ef6e1500355425fb3aa9aa813176ac48e75
2jisu <jisu.jjang88@gmail.com>
2024-01-21 15:42:03 +0900

    [UPDATE] 04-01장 실습 코드 완료

--- Chapter04/01/src/App.tsx
+++ Chapter04/01/src/App.tsx
@@ -1,23 +1,34 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import Clock from './pages/Clock'
+import {useClock} from './hooks'
 
 export default function App() {
-  let today = new Date()
-
-  // useEffect 훅 사용법
-  // useEffect(콜백_함수, 의존성_목록)
-  // 콜백_함수 = () => {}
-  // 의존성 목록에 있는 조건 중 어느 하나라도 충족되면 그때마다 콜백 함수를 다시 실행함.
+  // let today = new Date()
+  // useRef 훅 사용 (여전히 화면은 갱신하지 않음 -> useRef 훅은 컴포넌트를 다시 렌더링하지 않기 때문)
+  // let today = useRef(new Date())
+  // useRef 대신 useState 훅을 사용하여 시계 완성
+  // const [today, setToday] = useState(new Date())
 
   // 화면이 자동으로 갱신하지 않는 버그 발생
-  useEffect(() => {
+  /* useEffect(() => {
     console.log('useEffect called.')
     const duration = 1000
     const id = setInterval(() => {
-      today = new Date()
-      console.log('today', today.toLocaleTimeString())
+      // today = new Date()
+      // useRef 훅 사용
+      //today.current = new Date()
+      // console.log('today', today.current.toLocaleTimeString())
+
+      // useRef 대신 useState 훅을 사용하여 시계 완성
+      setToday(new Date())
     }, duration)
     return () => clearInterval(id)
-  }, [])
+  }, []) */
+  // useRef 훅 사용
+  // return <Clock today={today.current} />
+
+  // useClock 커스텀 훅으로 만든 시계
+  const today = useClock()
+
   return <Clock today={today} />
 }
