28bb50ecf94ca9884927a49ebe9f7d0679d46818
2jisu <jisu.jjang88@gmail.com>
2024-01-25 21:52:50 +0900

    [UPDATE] 04-03장 실습 코드 완료

--- Chapter04/03/src/pages/BasicForm.tsx
+++ Chapter04/03/src/pages/BasicForm.tsx
@@ -1,8 +1,70 @@
-export default function CopyMe() {
+import type {FormEvent, ChangeEvent} from 'react'
+import {useCallback, useState} from 'react'
+import {Title} from '../components'
+
+export default function BasicForm() {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault() // 중요! e.preventDefault를 호출해야 페이지가 다시 렌더링되지 않음
+      const formData = new FormData()
+      formData.append('name', name)
+      formData.append('email', email)
+      alert(JSON.stringify(Object.fromEntries(formData), null, 2))
+    },
+    [name, email]
+  )
+
+  const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setName(notUsed => e.target.value)
+  }, [])
+  const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(notUsed => e.target.value)
+  }, [])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>BasicForm</Title>
+      <div className="flex justify-center mt-4">
+        <form onSubmit={onSubmit}>
+          <div className="form-control">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Username</span>
+            </label>
+            <input
+              value={name}
+              onChange={onChangeName}
+              id="name"
+              type="text"
+              placeholder="enter your name"
+              className="input input-primary"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="email">
+              <span className="label-text">email</span>
+            </label>
+            <input
+              value={email}
+              onChange={onChangeEmail}
+              id="name"
+              type="text"
+              placeholder="enter your email"
+              className="input input-primary"
+            />
+          </div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="submit"
+              value="submit"
+              className="w-1/2 btn btn-sm btn-primary"
+            />
+            <input defaultValue="Cancel" className="w-1/2 ml-4 btn btn-sm" />
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
--- Chapter04/03/src/pages/ObjectState.tsx
+++ Chapter04/03/src/pages/ObjectState.tsx
@@ -1,8 +1,72 @@
-export default function CopyMe() {
+import type {FormEvent, ChangeEvent} from 'react'
+import {useCallback, useState} from 'react'
+import {Title} from '../components'
+import {StringDecoder} from 'string_decoder'
+
+// 객체 타입 값일 때 useState 훅 사용하기
+type FormType = {
+  name: string
+  email: string
+}
+export default function ObjectState() {
+  const [form, setForm] = useState<FormType>({name: '', email: ''})
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault() // 중요! e.preventDefault를 호출해야 페이지가 다시 렌더링되지 않음
+      alert(JSON.stringify(form, null, 2))
+    },
+    [form]
+  )
+
+  const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setForm(state => ({...state, name: e.target.value})) // 타입스크립트에서 객체를 반환하는 구문은 ({}) 형태로 사용해야 함
+  }, [])
+  const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setForm(state => ({...state, email: e.target.value}))
+  }, [])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ObjectState</Title>
+      <div className="flex justify-center mt-4">
+        <form onSubmit={onSubmit}>
+          <div className="form-control">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Username</span>
+            </label>
+            <input
+              value={form.name}
+              onChange={onChangeName}
+              id="name"
+              type="text"
+              placeholder="enter your name"
+              className="input input-primary"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="email">
+              <span className="label-text">email</span>
+            </label>
+            <input
+              value={form.email}
+              onChange={onChangeEmail}
+              id="name"
+              type="text"
+              placeholder="enter your email"
+              className="input input-primary"
+            />
+          </div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="submit"
+              value="submit"
+              className="w-1/2 btn btn-sm btn-primary"
+            />
+            <input defaultValue="Cancel" className="w-1/2 ml-4 btn btn-sm" />
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
