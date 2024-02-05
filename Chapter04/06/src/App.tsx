6cffb72547e144d662458b3a2f894b18d1885369
2jisu <jisu.jjang88@gmail.com>
2024-02-05 13:25:53 +0900

    [ADD] 04-06장 실습 코드 추가

--- Chapter04/06/src/App.tsx
+++ Chapter04/06/src/App.tsx
@@ -1,19 +1,9 @@
-import ClickTest from './pages/ClickTest'
-import FileDrop from './pages/FileDrop'
-import InputFocusTest from './pages/InputFocusTest'
-import InputValueTest from './pages/InputValueTest'
-import ForwardRefTest from './pages/ForwardRefTest'
-import ValidatableInputTest from './pages/ValidatableInputTest'
+import ResponsiveContextTest from './pages/ResponsiveContextTest'
 
 export default function App() {
   return (
     <main>
-      <ValidatableInputTest />
-      <ForwardRefTest />
-      <InputValueTest />
-      <InputFocusTest />
-      <FileDrop />
-      <ClickTest />
+      <ResponsiveContextTest />
     </main>
   )
 }
